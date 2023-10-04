39b3aa467d62837e31efd7492034faed0790231b
AndreyMachula <amcz2020@gmail.com>
2023-10-05 01:09:45 +0200

    fix

--- js/app.js
+++ js/app.js
@@ -1,23 +1,47 @@
-window.addEventListener('deviceorientation', handleOrientation);
-
-document.addEventListener('mousemove', e => {
-    Object.assign(document.documentElement, {
-        style: `
-        --move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
-        --move-y: ${(e.clientY - window.innerHeight / 2) * -.01}deg;
-        `
-    })
-})
-
-
+document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('deviceorientation', handleOrientation);
+    window.addEventListener('mousemove', handleMouseMove);
+});
 
 function handleOrientation(event) {
-    const beta = event.beta || 0; // Угол наклона устройства по оси X
-    const gamma = event.gamma || 0; // Угол наклона устройства по оси Y
+    const beta = event.beta || 0;
+    const gamma = event.gamma || 0;
 
-    const moveX = (gamma - 90) * -0.5; // Пересчет для оси X
-    const moveY = (beta - 90) * -1; // Пересчет для оси Y
+    const moveX = (gamma - 90) * -0.5;
+    const moveY = (beta - 90) * -1;
 
     document.documentElement.style.setProperty('--move-x', `${moveX}deg`);
     document.documentElement.style.setProperty('--move-y', `${moveY}deg`);
 }
+
+function handleMouseMove(e) {
+    const moveX = (e.clientX - window.innerWidth / 2) * -0.005;
+    const moveY = (e.clientY - window.innerHeight / 2) * -0.01;
+
+    document.documentElement.style.setProperty('--move-x', `${moveX}deg`);
+    document.documentElement.style.setProperty('--move-y', `${moveY}deg`);
+}
+
+// window.addEventListener('deviceorientation', handleOrientation);
+
+// document.addEventListener('mousemove', e => {
+//     Object.assign(document.documentElement, {
+//         style: `
+//         --move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
+//         --move-y: ${(e.clientY - window.innerHeight / 2) * -.01}deg;
+//         `
+//     })
+// })
+
+
+
+// function handleOrientation(event) {
+//     const beta = event.beta || 0; // Угол наклона устройства по оси X
+//     const gamma = event.gamma || 0; // Угол наклона устройства по оси Y
+
+//     const moveX = (gamma - 90) * -0.5; // Пересчет для оси X
+//     const moveY = (beta - 90) * -1; // Пересчет для оси Y
+
+//     document.documentElement.style.setProperty('--move-x', `${moveX}deg`);
+//     document.documentElement.style.setProperty('--move-y', `${moveY}deg`);
+// }
