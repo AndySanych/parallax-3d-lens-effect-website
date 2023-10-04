582eb5f1002528055f7b414f6fe16f35ac5b748c
AndreyMachula <amcz2020@gmail.com>
2023-10-05 00:53:31 +0200

    added deviceorientation

--- js/app.js
+++ js/app.js
@@ -6,3 +6,16 @@ document.addEventListener('mousemove', e => {
         `
     })
 })
+
+window.addEventListener('deviceorientation', handleOrientation);
+
+function handleOrientation(event) {
+    const beta = event.beta || 0; // Угол наклона устройства по оси X
+    const gamma = event.gamma || 0; // Угол наклона устройства по оси Y
+
+    const moveX = (gamma - 90) * -0.5; // Пересчет для оси X
+    const moveY = (beta - 90) * -1; // Пересчет для оси Y
+
+    document.documentElement.style.setProperty('--move-x', `${moveX}deg`);
+    document.documentElement.style.setProperty('--move-y', `${moveY}deg`);
+}
