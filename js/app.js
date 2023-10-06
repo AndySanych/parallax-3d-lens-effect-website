ead1610c2834fc2a4fafbdc0c319925fa47bc586
AndreyMachula <amcz2020@gmail.com>
2023-10-06 22:58:31 +0200

    added multiplier to reduce sensitivity

--- js/app.js
+++ js/app.js
@@ -7,8 +7,11 @@ function handleOrientation(event) {
     const beta = event.beta || 0;
     const gamma = event.gamma || 0;
 
-    const moveX = (gamma - 90) * -0.01;
-    const moveY = (beta - 90) * -0.9;
+    // Множитель для уменьшения чувствительности
+    const sensitivityMultiplier = 0.3;
+
+    const moveX = (gamma - 90) * -0.5 * sensitivityMultiplier;
+    const moveY = (beta - 90) * -1 * sensitivityMultiplier;
 
     document.documentElement.style.setProperty('--move-x', `${moveX}deg`);
     document.documentElement.style.setProperty('--move-y', `${moveY}deg`);
